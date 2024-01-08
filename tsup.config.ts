b518e1c1fdadb637d1aff085ac240b0b13f74e31
N <34268416+ziloen@users.noreply.github.com>
2024-01-08 16:52:49 +0800

    chore: update build

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,8 +3,11 @@ import { defineConfig } from "tsup"
 export default defineConfig({
   entry: [
     "src/index.ts",
-    "src/background-forward.ts"
+    "src/background-forward.ts",
   ],
   outDir: "dist",
-  dts: true
+  minify: true,
+  dts: true,
+  format: ["esm"],
+  clean: true,
 })
