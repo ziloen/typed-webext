fe70f910dc046ecd890a811e6a1a2420625b6334
N <34268416+ziloen@users.noreply.github.com>
2024-04-09 12:27:20 +0800

    Fix build

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,16 +3,12 @@ import { defineConfig } from "tsup"
 export default defineConfig({
   entry: [
     "src/index.ts",
-    "src/message.ts",
-    "src/storage.ts",
-    "src/stream.ts"
   ],
   outDir: "dist",
   // minify: true,
   dts: true,
   format: ["esm"],
   platform: "node",
-  bundle: false,
   treeshake: true,
   sourcemap: true,
   clean: true,
