b87581998619cfa7b0eaeaeb60421de39962976e
N <34268416+ziloen@users.noreply.github.com>
2026-05-14 17:13:10 +0800

    chore: update build config

--- tsdown.config.js
+++ tsdown.config.js
@@ -3,11 +3,17 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   entry: ['src/index.ts', 'src/react.ts'],
   outDir: 'dist',
-  // minify: true,
   dts: {
     oxc: true,
   },
-  // attachDebugInfo: "none",
+  inputOptions: {
+    experimental: {
+      attachDebugInfo: "none",
+    }
+  },
+  outputOptions: {
+    comments: false,
+  },
   format: ['esm'],
   platform: 'node',
   treeshake: true,
